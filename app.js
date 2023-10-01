3bf92b59b6ed610f313075f5f6b3bafd441430f8
Stewart Gliniecki <stewgliniecki@gmail.com>
2023-10-01 21:34:48 +0100

    Interface design

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const player = (player, option) =>{
-    let playerInfo =() => console.log('Hi my name is' + `${player}`)
-    return {player, option, playerInfo}
+    let array = [];
+    let playerInfo =() => console.log('Hi my name is' + `${player}`);
+    return {player, option,array, playerInfo}
 }
 
-const gameboard = (() => {})
+const gameboard = (() => {
+
+    const winningSequences = 
+    [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+}
+)();
